658804bd807ce6e6e5cd4cc72abc782a47432c2a
anshyyy <anshuman9998@gmail.com>
2022-12-19 12:38:52 +0530

    Minor fixes in association

--- src/models/airport.js
+++ src/models/airport.js
@@ -10,9 +10,11 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      this.belongsTo(models.City);
-      foreignKey: 'cityId';
-      onDelete: 'CASCADE'
+      // define association here
+      this.belongsTo(models.City, {
+        foreignKey: 'cityId',
+        onDelete: 'CASCADE'
+      });
     }
   }
   Airport.init({
